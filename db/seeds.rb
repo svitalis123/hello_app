a9e6b6241068203f811d0e229bd3a74975b94578
vitalis <vitalismutwiri2gmail.com>
2023-03-01 10:04:28 +0300

    Changes to db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello, world!",
+  "Greeting!",
+  "Hey there",
+  "Hey!",
+  "Hello!"
+]
+
+greetings.each do |message|
+  Message.create!(context: message)
+end
